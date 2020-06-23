6e755eaaae93a1a270ecd9e91bf14e396289b981
baiyutang <irichardwang@gmail.com>
2020-06-23 11:28:35 +0800

    update config mode

--- src/Config.php
+++ src/Config.php
@@ -9,11 +9,10 @@ namespace DingTalkRobot;
 class Config
 {
     /**
-     * TODO: 这里要替换成自己的机器人token
      * @var static array $groups token配置组
      */
     public static $groups = array(
-        'default' => '9721a7444fbea2032eb316c9e40fe8c27df8ae3178ca514d0e9b47386d97f4c0',
-        'other' => '9721a7444fbea2032eb316c9e40fe8c27df8ae3178ca514d0e9b47386d97f4c0',
+        'default' => '***',
+        'other' => '***',
     );
 }
